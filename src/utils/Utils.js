48f92e9edf0289b6adfd9ef3f2bd0496336d2fd1
Roshiahsu <103357724+Roshiahsu@users.noreply.github.com>
2023-02-13 13:51:24 +0800

    新增 jwt 驗證方法

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,26 @@
+import {MessageBox, Message} from 'element-ui'
+import VueRouter from "vue-router";
+
 export function getUrl() {
     let url = "http://localhost:9080"
     return url;
+}
+
+export function reLogin() {
+    MessageBox.alert('用戶尚未入，請先登入？', '尚未登入', {
+        confirmButtonText: '確定',
+        callback: action => {
+            location.href = "/login"
+        }
+    })
+}
+
+export function haveJwt(jwt) {
+    if (jwt === null) {
+        console.log("沒有jwt準備跳轉")
+        reLogin()
+        return
+    }
 }
 
 // module.exports = getUrl;
